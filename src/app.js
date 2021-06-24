649e74e07164f059d5c9bbec8b135df4e0c86411
Mukit Hasan <mukit.hasan07@gmail.com>
2021-06-24 10:59:48 +0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,95 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//define paths for Express config
+const publicDirPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
+app.use(express.static(publicDirPath))
+
+//re
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Mukit'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Mukit'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Me',
+        name: 'Mukit'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Mukit',
+        errorMsg: 'Help Article Not Found'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    const address = req.query.address
+    if (!address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+    
+    geocode(address, (error, {latitude, longitude, location} = {}) => {
+        if(error) {
+            return res.send({error})
+        }
+    
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return res.send({error})
+            }
+            res.send({location,forecastData})
+        })
+    })
+})
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+        
+    }
+    res.send({
+        product:[]
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Mukit',
+        errorMsg: 'Page Not Found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('sever is up on port 3000')
+})
