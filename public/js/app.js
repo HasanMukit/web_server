6e075c540e5bc0a58fc9b1feccf5937516e5495d
Mukit Hasan <mukit.hasan07@gmail.com>
2021-06-24 13:29:43 +0600

    section 8 final update

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,63 @@
-console.log('Client Side JS')
+
+
+
+const button = document.querySelector('button')
+const search = document.querySelector('input')
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    const address = search.value
+    fetch('http://localhost:3000/weather?address='+address).then((response) => {
+    response.json().then((data) => {
+        if(data.error)
+        {
+            const elem = document.createElement('p')
+            elem.innerText = data.error
+            const target = document.querySelector('form')
+            target.parentNode.insertBefore(elem, target.nextSibling)
+            return
+        }
+        console.log(data.location)
+        
+        let forecast = {location: data.location, ...data.forecastData}
+        
+        
+        
+        console.log(forecast)
+        const headers = Object.keys(forecast)
+        
+        console.log(headers)
+        const table = document.querySelector('table')
+        table.innerHTML =""
+        const tbody = document.createElement('tbody')
+
+        for (let i = 0; i < headers.length; i++) {
+            let row = document.createElement('tr')
+            let cell1 = document.createElement('td')
+            cell1.textContent = headers[i]
+            row.appendChild(cell1)
+            let cell2 = document.createElement('td')
+            if (i === 4)
+            {
+             const icon = document.createElement('img')  
+             icon.src = forecast[headers[i]]
+             icon.width = '30'
+             cell2.appendChild(icon)
+            }
+            else {
+                cell2.textContent = forecast[headers[i]]
+            }
+
+            
+            row.appendChild(cell2)
+            tbody.appendChild(row)
+        }
+        table.appendChild(tbody)
+
+        
+
+
+    })
+})
+    
+})
